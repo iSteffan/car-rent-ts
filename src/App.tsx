1feef5f6784c23125be69013347b416dce25d84c
Stepan Skichko <st.skichko@gmail.com>
2023-10-12 13:42:55 +0300

    fix redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { lazy } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
@@ -20,25 +19,5 @@ const App = () => {
     </Routes>
   );
 };
-// import { Route, Routes } from 'react-router-dom';
-// import { lazy } from 'react';
-// import { Layout } from './Layout/Layout';
-
-// const Home = lazy(() => import('../pages/Home'));
-// const Catalog = lazy(() => import('../pages/Catalog'));
-// const Favorites = lazy(() => import('../pages/Favorites'));
-
-// export const App = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<Home />} />
-//         <Route path="/catalog" element={<Catalog />} />
-//         <Route path="/favorites" element={<Favorites />} />
-//         <Route path="*" element={<Home />} />
-//       </Route>
-//     </Routes>
-//   );
-// };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
-// import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <BrowserRouter basename="/">
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
-    </BrowserRouter>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <BrowserRouter basename="/">
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
