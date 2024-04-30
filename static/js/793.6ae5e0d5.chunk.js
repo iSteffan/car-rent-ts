a2c344251a507bca139e9c7221245cd588732a8c
Stepan Skichko <st.skichko@gmail.com>
2024-04-30 14:31:32 +0300

    Updates

--- static/js/793.6ae5e0d5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rent_ts=self.webpackChunkcar_rent_ts||[]).push([[793],{5793:function(n,t,e){e.r(t),e.d(t,{default:function(){return p}});var r,i=e(9434),c=e(5676),s=e(7478),u=e(2206),a=e(168),l=e(5867).ZP.p(r||(r=(0,a.Z)(["\n  margin-top: 60px;\n\n  text-align: center;\n"]))),f=e(184),p=function(){var n=(0,i.v9)(c.Tc);return(0,f.jsx)(u.W,{children:n.length>0?(0,f.jsx)(u.a,{children:n.map((function(n){return(0,f.jsx)(s._,{item:n},n.id)}))}):(0,f.jsx)(l,{children:"Your favorite list is empty"})})}}}]);
-//# sourceMappingURL=793.6ae5e0d5.chunk.js.map
